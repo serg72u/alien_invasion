1f80e00b5f6f30c13dca5f44b43b7f34eb3d0756
Sergey Utkin <serg72u@mail.ru>
2022-09-14 20:54:15 +0300

    Отрисовка корабля на экране.

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,6 +2,8 @@ import sys
 
 import pygame
 from settings import Settings
+from ship import Ship
+
 
 class AlienInvasion:
     """Класс для управления ресурсами и поведением игры."""
@@ -15,6 +17,8 @@ class AlienInvasion:
             (self.settings.screen_width, self.settings.screen_width))
         pygame.display.set_caption("Alien Invasion")
 
+        self.ship = Ship(self)
+
     def run_game(self):
         """Запуск основного цикла игры."""
         while True:
@@ -24,6 +28,7 @@ class AlienInvasion:
                     sys.exit()
             # При каждом проходе цикла перерерисовыветсч экран.
             self.screen.fill(self.settings.bg_color)
+            self.ship.blitme()
 
             # Отображение последнего прорисованного экрана.
             pygame.display.flip()
