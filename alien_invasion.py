576bb9555a79778f5b00079d65e900729a926bf6
Sergey Utkin <serg72u@mail.ru>
2022-09-14 20:34:22 +0300

    Добавлен класс настроек в модуле settings

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,7 +1,7 @@
 import sys
 
 import pygame
-
+from settings import Settings
 
 class AlienInvasion:
     """Класс для управления ресурсами и поведением игры."""
@@ -9,11 +9,11 @@ class AlienInvasion:
     def __init__(self):
         """Инициализирует игру и создает игровые ресурсы. """
         pygame.init()
+        self.settings = Settings()
 
-        self.screen = pygame.display.set_mode((1200, 800))
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_width))
         pygame.display.set_caption("Alien Invasion")
-        # Назначение цвета фона.
-        self.bg_color = (230, 230, 230)
 
     def run_game(self):
         """Запуск основного цикла игры."""
@@ -23,7 +23,7 @@ class AlienInvasion:
                 if event.type == pygame.QUIT:
                     sys.exit()
             # При каждом проходе цикла перерерисовыветсч экран.
-            self.screen.fill(self.bg_color)
+            self.screen.fill(self.settings.bg_color)
 
             # Отображение последнего прорисованного экрана.
             pygame.display.flip()
