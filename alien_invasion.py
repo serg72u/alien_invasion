49c62d543bacc191ce1f0968a8b65be1ae705022
Sergey Utkin <serg72u@mail.ru>
2022-09-15 20:38:07 +0300

    Перемещение на 1 пиксель в право.

--- alien_invasion.py
+++ alien_invasion.py
@@ -30,6 +30,10 @@ class AlienInvasion:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    # Переместить корабль вправо.
+                    self.ship.rect.x += 1
 
     def _update_screen(self):
         """Обновляет изображение на экране и отображает новый экран."""
