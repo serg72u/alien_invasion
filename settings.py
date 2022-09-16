c094ec59a904872b5a0b4115130e258a534c07d0
Sergey Utkin <serg72u@mail.ru>
2022-09-16 20:30:01 +0300

    Ограничение перемещений

--- settings.py
+++ settings.py
@@ -7,3 +7,7 @@ class Settings():
         self.screen_width = 1200
         self.screen_height = 800
         self.bg_color = (230, 230, 230)
+
+        # Настройки коробля
+        self.ship_speed = 1.5
+
--- ship.py
+++ ship.py
@@ -7,6 +7,7 @@ class Ship():
     def __init__(self, ai_game):
         """Инициализирует корабль и задает его начальную позицию."""
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
 
         # Загружаем изображение корабля и получает прямоугольник.
@@ -14,16 +15,23 @@ class Ship():
         self.rect = self.image.get_rect()
         # Каждый новый корабль появляется у нижнего края экрана.
         self.rect.midbottom = self.screen_rect.midbottom
+        # Сохранение вещественной координаты центра коробля.
+        self.x = float(self.rect.x)
 
         # Флаг перемещения
         self.moving_right = False
         self.moving_left = False
+
+
     def update(self):
         """Обновляет позицию корабля с учетом флагов."""
-        if self.moving_right:
-            self.rect.x += 1
-        if self.moving_left:
-            self.rect.x -= 1
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+        if self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.ship_speed
+
+        # Обновление атрибута rect на основании self.x
+        self.rect.x = self.x
 
     def blitme(self):
         """рисует корабль в текущей позиции."""
