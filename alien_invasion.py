a0ff293f396bc4b15ce6c1fdeee5fc9a21fd847d
Sergey Utkin <serg72u@mail.ru>
2022-09-16 21:00:00 +0300

    Delete FullScreen

--- alien_invasion.py
+++ alien_invasion.py
@@ -14,7 +14,7 @@ class AlienInvasion:
         self.settings = Settings()
 
         self.screen = pygame.display.set_mode(
-            (self.settings.screen_width, self.settings.screen_width))
+            (self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
 
         self.ship = Ship(self)
