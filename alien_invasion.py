4e4ac65fd2cf4b07f83636ab594da190e05fcb68
Sergey Utkin <serg72u@mail.ru>
2022-09-19 16:17:26 +0300

    Добавлено перемещение корабля вверх и вниз

--- alien_invasion.py
+++ alien_invasion.py
@@ -43,6 +43,10 @@ class AlienInvasion:
             self.ship.moving_right = True
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = True
+        elif event.key == pygame.K_UP:
+            self.ship.moving_up = True
+        elif event.key == pygame.K_DOWN:
+            self.ship.moving_down = True
         elif event.key == pygame.K_q:
             sys.exit()
 
@@ -52,6 +56,11 @@ class AlienInvasion:
             self.ship.moving_right = False
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = False
+        elif event.key == pygame.K_UP:
+            self.ship.moving_up = False
+        elif event.key == pygame.K_DOWN:
+            self.ship.moving_down = False
+
 
     def _update_screen(self):
         """Refreshes the screen image and displays the new screen."""
--- ship.py
+++ ship.py
@@ -17,10 +17,13 @@ class Ship():
         self.rect.midbottom = self.screen_rect.midbottom
         # Сохранение вещественной координаты центра коробля.
         self.x = float(self.rect.x)
+        self.y = float(self.rect.y)
 
         # Флаг перемещения
         self.moving_right = False
         self.moving_left = False
+        self.moving_up = False
+        self.moving_down = False
 
 
     def update(self):
@@ -29,9 +32,14 @@ class Ship():
             self.x += self.settings.ship_speed
         if self.moving_left and self.rect.left > 0:
             self.x -= self.settings.ship_speed
-
+        if self.moving_up and self.rect.top > self.screen_rect.top:
+            self.y -= self.settings.ship_speed
+        if self.moving_down and self.rect.bottom < self.screen_rect.bottom:
+            self.y += self.settings.ship_speed
         # Обновление атрибута rect на основании self.x
         self.rect.x = self.x
+        self.rect.y = self.y
+
 
     def blitme(self):
         """рисует корабль в текущей позиции."""
